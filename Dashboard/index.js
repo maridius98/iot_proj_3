9a2bb20463cf6d2c71ff7a852aa0f0978edbc13f
maridius98 <maridius98@yahoo.com>
2024-06-28 14:43:09 +0200

    refactored dashboard to use influxdb2.0

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,26 +1,35 @@
 import { connect, JSONCodec } from "nats";
 import Influx from "influx";
+import { InfluxDB, Point } from "@influxdata/influxdb-client";
+const token = 'yp1TduBCbqoAbIpemw62O5QpyNshnORrpaFy7Ji-6uEQJRhQaAnOXEqfd48pT4QGOOvR47-086jDkH8zZfiSbw=='
+const org = 'elfak'
+const bucket = 'mydb'
 
+const client = new InfluxDB({url: 'http://influxdb:8086', token: token})
+const writeApi = client.getWriteApi(org, bucket)
+writeApi.useDefaultTags({host: 'influxdb'})
 const natsConnection = {
   "servers": "nats://nats:4222"
 }
 
-const influx = new Influx.InfluxDB({
-  host: 'influxdb',
-  database: 'mydb',
-  schema: [
-    {
-      measurement: 'average_values',
-      fields: {
-        avgGlobalActivePower: Influx.FieldType.FLOAT,
-        avgGlobalReactivePower: Influx.FieldType.FLOAT,
-        avgVoltage: Influx.FieldType.FLOAT,
-        avgGlobalIntensity: Influx.FieldType.FLOAT,
-      },
-      tags: ['timestamp']
-    }
-  ]
-});
+
+// const influx = new Influx.InfluxDB({
+//   host: 'influxdb',
+//   database: 'mydb',
+//   token: 'yp1TduBCbqoAbIpemw62O5QpyNshnORrpaFy7Ji-6uEQJRhQaAnOXEqfd48pT4QGOOvR47-086jDkH8zZfiSbw==',
+//   schema: [
+//     {
+//       measurement: 'average_values',
+//       fields: {
+//         avgGlobalActivePower: Influx.FieldType.FLOAT,
+//         avgGlobalReactivePower: Influx.FieldType.FLOAT,
+//         avgVoltage: Influx.FieldType.FLOAT,
+//         avgGlobalIntensity: Influx.FieldType.FLOAT,
+//       },
+//       tags: ['timestamp']
+//     }
+//   ]
+// });
 
 const jc = JSONCodec()
 
@@ -32,21 +41,16 @@ const main = async () => {
     const data = jc.decode(m.data)
     console.log(data)
     console.log(`avgVoltage: ${data.avgVoltage}`)
-    influx.writePoints([
-      {
-        measurement: 'average_values',
-        tags: { timestamp: data.timestamp },
-        fields: {
-          avgGlobalActivePower: data.avgGlobalActivePower,
-          avgGlobalReactivePower: data.avgGlobalReactivePower,
-          avgVoltage: data.avgVoltage,
-          avgGlobalIntensity: data.avgGlobalIntensity,
-        },
+    const point = new Point('average_values')
+      .tag('timestamp', data.timestamp)
+      .floatField('avgGlobalActivePower', data.avgGlobalActivePower)
+      .floatField('avgGlobalReactivePower', data.avgGlobalReactivePower)
+      .floatField('avgVoltage', data.avgVoltage)
+      .floatField('avgGlobalIntensity', data.avgGlobalIntensity);
+      writeApi.writePoint(point)
       }
-    ]).catch(err => {
-      console.error(`Error saving data to InfluxDB! ${err.stack}`)
-    });
-  }
+
+      
   console.log("subscription closed");
 }
 
